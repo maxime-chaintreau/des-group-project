b800d29dbfd666b9f964c1d5c5821e642e1f18c1
maxime-chaintreau <illusionsoul91@gmail.com>
2026-05-30 02:22:03 -0400

    fix: trust proxy for rate limiter on Render

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+app.set("trust proxy", 1)
 const { createServer } = require("http");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
